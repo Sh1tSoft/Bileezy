ef314bf3bc02c063f5d3535f3213c8cc479f3ed8
LordCrisp <lord_crisp@hotmail.com>
2018-01-08 09:24:30 +0100

    :unicorn: små ændringer til index, header, footer

--- includes/header.php
+++ includes/header.php
@@ -9,21 +9,20 @@ session_start();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta name="theme-color" content="#212121" id="theme_color">
-    <title>CinePhile</title>
+    <meta name="theme-color" content="#E91E63" id="theme_color">
+    <title>Bileezy</title>
     <!-- Bootstrap stylesheet -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <!-- Custom stylesheet -->
     <link rel="stylesheet" href="assets/styles/css/master.css">
-    <!-- Fonts -->
-    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500|Righteous">
     <!-- Material icons -->
     <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
 </head>
-<body>
 
-<!-- Header start -->
+<!-- BODY (start) -->
+<body>
+<!-- HEADER (start) -->
 <header class="header__main">
     <a class="header__logo" href="index.php"><img src="assets/app/logo.svg"><h1>CinePhile</h1></a>
 </header>
-<!-- Header end -->
+<!-- Header (end) -->
---
+++ index.php
@@ -0,0 +1,6 @@
+<!-- HEADER & BODY (start) -->
+<?php require 'includes/header.php'; ?>
+
+
+<!-- FOOTER & BODY (end) -->
+<?php require 'includes/footer.php'; ?>
