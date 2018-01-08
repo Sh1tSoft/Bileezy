65a775fef604046e585a3a48494d67981df5ffa9
MathiasMogensen <mathiasgulbaek@hotmail.dk>
2018-01-08 12:26:58 +0100

    hej :)

--- create/carmodel.php
+++ create/carmodel.php
@@ -9,6 +9,17 @@ include '../includes/dbh.inc.php';
 include '../includes/createcarmodel.php';
 ?>
 
+<a href="?create">Create Model</a><br><br>
+
+
+
+                                                                <!------------------------------------------------------ CREATE MODEL ------------------------------------------------------>
+
+
+
+
+<?php if(isset($_GET['create'])) : ?>
+
 <form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
     <label for="brand">Brand</label>
     <select name="brand">
@@ -160,3 +171,229 @@ include '../includes/createcarmodel.php';
     <input type="text" name="year_end">
     <button type="submit" name="submit">Submit</button>
 </form>
+
+
+
+
+
+
+
+                                                                <!------------------------------------------------------ EDIT MODEL ------------------------------------------------------>
+
+
+
+
+
+
+
+
+
+<?php elseif(isset($_GET['edit'])) : 
+    $id = $_GET['edit'];
+    $sqlall = "SELECT * FROM car_model_info WHERE id = $id";
+    $resultall = $conn->query($sqlall);
+?>
+
+<?php if ($rowall = $resultall->fetch_assoc()) : ?>
+
+<form action="<?= htmlspecialchars($_SERVER["PHP_SELF"]) ?>" method="POST">
+    <label for="brand">Brand</label>
+    <select name="brand">
+        <?php 
+            $sql = "SELECT * FROM car_brands";
+            $result = $conn->query($sql);
+        ?>
+        <option value=""><?=$rowall['brand']?></option>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <option value="<?=$row['brand']?>"><?=$row['brand']?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="model_name">Model Name</label>
+    <input type="text" value="<?=$rowall['model']?>" name="model_name">
+    <label for="body_type">Body Type</label>
+    <select name="body_type">
+        <?php 
+            $sql = "SELECT * FROM car_body_type";
+            $result = $conn->query($sql);
+        ?>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <option value="<?=$row['body_type']?>"><?=$row['body_type']?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="equipment_model">Equipment Model</label>
+    <input type="text" value="<?=$rowall['equipment_model']?>" name="equipment_model">
+    <label for="engine_size">Engine Size (Liter with decimal)</label>
+    <input type="text" value="<?=$rowall['engine_size']?>" name="engine_size">
+    <label for="engine_type">Engine Type Model</label>
+    <select name="engine_type">
+        <?php 
+            $sql = "SELECT * FROM engine_type";
+            $result = $conn->query($sql);
+        ?>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <option value="<?=$row['engine_type']?>"><?=$row['engine_type']?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="horsepower">Horsepower</label>
+    <input type="text" value="<?=$rowall['horsepower']?>" name="horsepower">
+    <label for="torque">Torque</label>
+    <input type="text" value="<?=$rowall['torque']?>" name="torque">
+    <label for="consumption">Consumption (KM/L)</label>
+    <input type="text" value="<?=$rowall['consumption']?>" name="consumption">
+    <label for="top_speed">Top Speed (KM/H)</label>
+    <input type="text" value="<?=$rowall['top_speed']?>" name="top_speed">
+    <label for="acceleration">Acceleration (0-100 KM/H in seconds)</label>
+    <input type="text" value="<?=$rowall['acceleration']?>" name="acceleration">
+    <label for="transmission_type">Transmission Type</label>
+    <select name="transmission_type">
+        <?php 
+            $sql = "SELECT * FROM transmission_type";
+            $result = $conn->query($sql);
+        ?>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <option value="<?=$row['transmission_type']?>"><?=$row['transmission_type']?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="gears">Gears</label>
+    <input type="text" value="<?=$rowall['gears']?>" name="gears">
+    <label for="new_price">New Price</label>
+    <input type="text" value="<?=$rowall['new_price']?>" name="new_price">
+    <label for="weight">Weight</label>
+    <input type="text" value="<?=$rowall['car_weight']?>" name="weight">
+    <label for="doors">Doors</label>
+    <select name="doors">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+    </select>
+    <label for="airbags">Airbags</label>
+    <select name="airbags">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+    </select>
+    <label for="cylinders">Cylinders</label>
+    <select name="cylinders">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+        <option value="11">11</option>
+        <option value="12">12</option>
+        <option value="13">13</option>
+        <option value="14">14</option>
+        <option value="15">15</option>
+        <option value="16">16</option>
+    </select>
+    <label for="drivetrain">Drivetrain</label>
+    <select name="drivetrain">
+        <?php 
+            $sql = "SELECT * FROM drivetrain";
+            $result = $conn->query($sql);
+        ?>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <option value="<?=$row['drivetrain']?>"><?=$row['drivetrain']?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="load_capacity">Load Capacity</label>
+    <input type="text" value="<?=$rowall['load_capacity']?>" name="load_capacity">
+    <label for="seats">Seats</label>
+    <select name="seats">
+        <option value="1">1</option>
+        <option value="2">2</option>
+        <option value="3">3</option>
+        <option value="4">4</option>
+        <option value="5">5</option>
+        <option value="6">6</option>
+        <option value="7">7</option>
+        <option value="8">8</option>
+        <option value="9">9</option>
+        <option value="10">10</option>
+    </select>
+    <label for="fuel_type">Fuel Type</label>
+    <select name="fuel_type">
+        <?php 
+            $sql = "SELECT * FROM fuel_type";
+            $result = $conn->query($sql);
+        ?>
+        <?php while ($row = $result->fetch_assoc()) : ?>
+            <option value="<?=$row['type']?>"><?=$row['type']?></option>
+        <?php endwhile; ?>
+    </select>
+    <label for="tank_capacity">Tank Capacity</label>
+    <input type="text" value="<?=$rowall['tank_capacity']?>" name="tank_capacity">
+    <label for="year_start">Start Year</label>
+    <input type="text" value="<?=$rowall['year_start']?>" name="year_start">
+    <label for="year_end">End Year</label>
+    <input type="text" value="<?=$rowall['year_end']?>" name="year_end">
+    <button type="submit" name="submit">Submit</button>
+</form>
+
+<?php endif; ?>
+
+
+
+
+
+                                                                <!------------------------------------------------------ LIST MODELS ------------------------------------------------------>
+
+
+
+
+
+
+<?php else : 
+$sql = "SELECT * FROM car_model_info";
+$result = $conn->query($sql);    
+?>
+
+<?php while ($row = $result->fetch_assoc()) : ?>
+    <?=$row['brand']?> |
+    <?=$row['model']?> |
+    <?=$row['car_body_type']?> |
+    <?=$row['equipment_model']?> |
+    <?=$row['engine_size']?> |
+    <?=$row['engine_type']?> |
+    <?=$row['horsepower']?> |
+    <?=$row['torque']?> |
+    <?=$row['consumption']?> |
+    <?=$row['top_speed']?> |
+    <?=$row['acceleration']?> |
+    <?=$row['transmission_type']?> |
+    <?=$row['gears']?> |
+    <?=$row['new_price']?> |
+    <?=$row['car_weight']?> |
+    <?=$row['doors']?> |
+    <?=$row['airbags']?> |
+    <?=$row['cylinders']?> |
+    <?=$row['drivetrain']?> |
+    <?=$row['load_capacity']?> |
+    <?=$row['seats']?> |
+    <?=$row['fuel_type']?> |
+    <?=$row['tank_capacity']?> |
+    <?=$row['year_start']?> |
+    <?=$row['year_end']?> |
+    <a href="?edit=<?=$row['id']?>">Edit</a>
+<?php endwhile; ?>
+
+<?php endif; ?>
---
+++ includes/updatecarmodel.php
@@ -0,0 +1,82 @@
+<?php
+    include_once 'dbh.inc.php';
+
+    if(isset($_POST['submit'])) {
+        if ($_SERVER["REQUEST_METHOD"] == "POST") {
+
+            $modelId
+        
+            $brand = mysqli_real_escape_string($conn, $_POST['brand']);
+            $modelName = mysqli_real_escape_string($conn, $_POST['model_name']);
+            $body_type = mysqli_real_escape_string($conn, $_POST['body_type']);
+            $equipment_model = mysqli_real_escape_string($conn, $_POST['equipment_model']);
+            $engine_size = mysqli_real_escape_string($conn, $_POST['engine_size']);
+            $engine_type = mysqli_real_escape_string($conn, $_POST['engine_type']);
+            $horsepower = mysqli_real_escape_string($conn, $_POST['horsepower']);
+            $torque = mysqli_real_escape_string($conn, $_POST['torque']);
+            $consumption = mysqli_real_escape_string($conn, $_POST['consumption']);
+            $top_speed = mysqli_real_escape_string($conn, $_POST['top_speed']);
+            $acceleration = mysqli_real_escape_string($conn, $_POST['acceleration']);
+            $transmission_type = mysqli_real_escape_string($conn, $_POST['transmission_type']);
+            $gears = mysqli_real_escape_string($conn, $_POST['gears']);
+            $new_price = mysqli_real_escape_string($conn, $_POST['new_price']);
+            $weight = mysqli_real_escape_string($conn, $_POST['weight']);
+            $doors = mysqli_real_escape_string($conn, $_POST['doors']);
+            $airbags = mysqli_real_escape_string($conn, $_POST['airbags']);
+            $cylinders = mysqli_real_escape_string($conn, $_POST['cylinders']);
+            $drivetrain = mysqli_real_escape_string($conn, $_POST['drivetrain']);
+            $load_capacity = mysqli_real_escape_string($conn, $_POST['load_capacity']);
+            $seats = mysqli_real_escape_string($conn, $_POST['seats']);
+            $fuel_type = mysqli_real_escape_string($conn, $_POST['fuel_type']);
+            $tank_capacity = mysqli_real_escape_string($conn, $_POST['tank_capacity']);
+            $year_start = mysqli_real_escape_string($conn, $_POST['year_start']);
+            $years_end = mysqli_real_escape_string($conn, $_POST['year_end']);
+
+                //Insert the user into the database
+                $sql = "UPDATE `car_model_info` SET 
+                `brand`=?,
+                `model`=?,
+                `car_body_type`=?,
+                `equipment_model`=?,
+                `engine_size`=?,
+                `engine_type`=?,
+                `horsepower`=?,
+                `torque`=?,
+                `consumption`=?,
+                `top_speed`=?,
+                `acceleration`=?,
+                `transmission_type`=?,
+                `gears`=?,
+                `new_price`=?,
+                `car_weight`=?,
+                `doors`=?,
+                `airbags`=?,
+                `cylinders`=?,
+                `drivetrain`=?,
+                `load_capacity`=?,
+                `seats`=?,
+                `fuel_type`=?,
+                `tank_capacity`=?,
+                `year_start`=?,
+                `year_end`=?,
+                WHERE id = $modelId";
+                //Create second prepared statement
+                $stmt2 = mysqli_stmt_init($conn);
+
+                //Check if prepared statement fails
+                if (!mysqli_stmt_prepare($stmt2, $sql)) {
+                    header("Location: ../index.php?error");
+                    exit();
+                } else {
+                    //Bind parameters to the placeholder
+                    mysqli_stmt_bind_param($stmt, "sssssssssssssssssssssssss", $brand, $modelName, $body_type, $equipment_model, $engine_size, $engine_type, $horsepower, $torque, $consumption, $top_speed, $acceleration, $transmission_type,
+                    $gears, $new_price, $weight, $doors, $airbags, $cylinders, $drivetrain, $load_capacity, $seats, $fuel_type, $tank_capacity, $year_start, $years_end);
+
+                    //Run query in database
+                    mysqli_stmt_execute($stmt2);
+                }
+            }
+        }
+                    $conn->close();
+                    header("Location: ".$doc_root."/admin/donors.php?edited");
+?>
