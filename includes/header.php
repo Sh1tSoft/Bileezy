f333988136cd083df66c7f2b0fdc7782eed3d5cd
LordCrisp <lord_crisp@hotmail.com>
2018-01-10 13:09:09 +0100

    css måske fikset.... :unicorn:

--- includes/header.php
+++ includes/header.php
@@ -1,5 +1,8 @@
 <?php
-require 'dbh.inc.php';
+    if (session_status() == PHP_SESSION_NONE) {
+        session_start();
+    }
+    require 'dbh.inc.php';
 ?>
 
 <!DOCTYPE html>
@@ -9,14 +12,15 @@ require 'dbh.inc.php';
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="theme-color" content="#E91E63" id="theme_color">
     <title><?=$pageTitle?> | Bileezy</title>
-    <!-- Font -->
-    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
     <!-- Bootstrap stylesheet -->
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/css/bootstrap.min.css">
     <!-- Custom stylesheet -->
     <link rel="stylesheet" href="<?=$root?>/assets/css/master.css">
     <!-- Google fonts -->
+    <link href="https://fonts.googleapis.com/css?family=Lato:300,400,700,900" rel="stylesheet">
     <link href="https://fonts.googleapis.com/css?family=Josefin+Sans:400,600" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+          rel="stylesheet">
     <!-- Favicon -->
     <link rel="icon" href="<?=$root?>/favicon.ico" type="image/x-icon"/>
     <link rel="shortcut icon" href="<?=$root?>/favicon.ico" type="image/x-icon"/>
@@ -41,12 +45,6 @@ require 'dbh.inc.php';
         </div>
         <!-- Account & Login -->
         <div class="header__account col-md-3">
-          <?php
-          if (session_status() == PHP_SESSION_NONE) {
-              session_start();
-          }
-          ?>
-
           <?php if (!empty($_SESSION['email'])) : ?>
           <?php else: ?>
               <a <?php if (basename($_SERVER['PHP_SELF']) == "login.php") { echo 'class="active-link"'; } ?> href="<?=$root?>/login.php">Opret Konto</a>
@@ -71,7 +69,7 @@ require 'dbh.inc.php';
     <!-- NAVBAR -->
     <section class="header__nav">
         <ul>
-            <li><a href="<?=$root?>index.php">Forside</a></li>
+            <li><a href="<?=$root?>/index.php">Forside</a></li>
             <li><a href="<?=$root?>/products.php">Bilannoncer</a></li>
             <li><a href="#">Om</a></li>
             <li><a href="#">Kontakt</a></li>
