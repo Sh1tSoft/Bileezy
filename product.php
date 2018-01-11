2f63e12c0a8d9d6ea403f17344383433a4e494b3
thobias12 <thobiasslet@live.dk>
2018-01-11 10:21:22 +0100

    :poop:

---
+++ product.php
@@ -0,0 +1,11 @@
+<?php
+$pageTitle = "Forside";
+require 'includes/header.php'; ?>
+<!-- HEADER & BODY (start) -->
+
+<section class="product">
+  <img src="" alt="">
+</section>
+
+<!-- FOOTER & BODY (end) -->
+<?php require 'includes/footer.php'; ?>
