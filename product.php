a8f5c73fab68d122b4033021c00fc68ee7559e5c
thobias12 <thobiasslet@live.dk>
2018-01-12 11:00:49 +0100

    :poop:

--- product.php
+++ product.php
@@ -3,14 +3,24 @@ $pageTitle = "Forside";
 require 'includes/header.php'; ?>
 <!-- HEADER & BODY (start) -->
 
+<?php
+
+ $query = "SELECT * FROM `car_model_info` WHERE ID='3' ";
+ $result = $conn->query($query);
+ $query2 = "SELECT * FROM `products` WHERE ID='3' ";
+ $result2 = $conn->query($query2);
+ ?>
+
+
 <section class="container">
   <div class="product-item">
       <img src="assets/img/tesla.jpg" alt="">
-
         <h6 class="car-info">
-          <span class="car-name">Tesla Model S</span>
+          <?php while($row = $result -> fetch_assoc()) : ?>
+            <?php while($row2 = $result2 -> fetch_assoc()) : ?>
+          <span class="car-name"><?=$row['brand']?> <?=$row['model']?></span>
           <span class="car-info-splitter">|</span>
-          <span class="car-price">1600000</span>
+          <span class="car-price"><?=$row2['price']?></span>
         </h6>
 
     <div class="keypoint-container">
@@ -74,8 +84,133 @@ require 'includes/header.php'; ?>
 
   </div>
 
+  <div class="specs col-lg-12">
+    <table class="table-fill">
 
+      <thead>
+        <tr>
+          <th class="text-left">Specifikationer</th>
+          <th class="text-left">Viste Bil</th>
+        </tr>
+      </thead>
+
+      <tbody class="table-hover">
+        <tr>
+          <td class="text-left">Mærke</td>
+          <td class="text-left"><?=$row['brand']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Model Navn</td>
+          <td class="text-left"><?=$row['model']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Karosseri Type</td>
+          <td class="text-left"><?=$row['car_body_type']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Udstyrs Model</td>
+          <td class="text-left"><?=$row['equipment_model']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Motor Størrelse i Liter</td>
+          <td class="text-left"><?=$row['engine_size']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Motor type</td>
+          <td class="text-left"><?=$row['engine_type']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Hestekræfter</td>
+          <td class="text-left"><?=$row['horsepower']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Moment</td>
+          <td class="text-left"><?=$row['torque']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Forbrug</td>
+          <td class="text-left"><?=$row['consumption']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Top hastighed</td>
+          <td class="text-left"><?=$row['top_speed']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Acceleration</td>
+          <td class="text-left"><?=$row['acceleration']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Gearkasse</td>
+          <td class="text-left"><?=$row['transmission_type']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Gear</td>
+          <td class="text-left"><?=$row['gears']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Ny Pris</td>
+          <td class="text-left"><?=$row['new_price']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Vægt</td>
+          <td class="text-left"><?=$row['weight']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Antal døre</td>
+          <td class="text-left"><?=$row['doors']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Antal airbags</td>
+          <td class="text-left"><?=$row['airbags']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Cylindere</td>
+          <td class="text-left"><?=$row['cylinders']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Trækhjul</td>
+          <td class="text-left"><?=$row['drivetrain']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Lastevne</td>
+          <td class="text-left"><?=$row['load_capacity']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Antal sæder</td>
+          <td class="text-left"><?=$row['seats']?></td>
+        </tr>
+
+        <tr>
+          <td class="text-left">Brændstof</td>
+          <td class="text-left"><?=$row['fuel_type']?></td>
+        </tr>
+      <?php endwhile; ?>
+    <?php endwhile; ?>
+      </tbody>
+    </table>
   </div>
+
+</div>
 </section>
 
 <!-- FOOTER & BODY (end) -->
